115e567f654b07e1085aa460b8c8d80ddf155e25
tinogomes <tinorj@gmail.com>
2012-03-29 18:50:08 -0300

    Readme and License

---
+++ README.mk
@@ -0,0 +1,23 @@
+# My RC Files
+
+## Requirements
+
+Z Shell - http://www.zsh.org/
+
+    $ brew install zsh # On OSX
+
+Oh my zsh - https://github.com/tinogomes/oh-my-zsh
+
+    $ git clone git://github.com/tinogomes/oh-my-zsh.git
+
+## How to install
+
+    $ cd $HOME
+    $ git clone git://github.com/tinogomes/rcfiles.git .rcfiles
+    $ .rcfiles/install
+
+## Local settings
+
+Create .zshrc\_local file at your ```$HOME```
+
+    $ touch ~/.zshrc_local
