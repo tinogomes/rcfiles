16860f798b1696a5285584e8712c28e724b93028
tinogomes <tinorj@gmail.com>
2020-08-13 23:29:41 -0300

    Replace git repo on install

--- install.sh
+++ install.sh
@@ -24,17 +24,17 @@ cd $HOME
 
 if [ ! -d $HOME/.oh-my-zsh ]; then
   echo "Getting OM MY ZSH from tinogomes' fork repository"
-  git clone https://github.com/tinogomes/ohmyzsh.git .oh-my-zsh || exit 1
+  git clone git@github.com:tinogomes/ohmyzsh.git .oh-my-zsh || exit 1
   mv .oh-my-zsh/custom{,-original}
   echo "Getting My OH-MY-ZSH custom plugin and theme"
-  git clone https://github.com/tinogomes/oh-my-zsh-custom.git .oh-my-zsh/custom || exit 1
+  git clone git@github.com:tinogomes/oh-my-zsh-custom.git .oh-my-zsh/custom || exit 1
   echo "Getting zsh-syntax-highlighting"
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting || exit 1
 fi
 
 if [ ! -d $HOME/.rcfiles ]; then
   echo "Getting RCFiles"
-  git clone https://github.com/tinogomes/rcfiles.git .rcfiles || exit 1
+  git clone git@github.com:tinogomes/rcfiles.git .rcfiles || exit 1
 fi
 
 if [ ! -d $HOME/.rcfiles ]; then
